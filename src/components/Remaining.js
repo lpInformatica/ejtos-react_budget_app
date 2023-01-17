800122da7e2f89b445ed390b6945a25c0bafb421
lpInformatica <info@lpinformatica.it>
2023-01-17 17:18:37 -0500

    Added a modified version of components/Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,15 +1,20 @@
-import React, {useContext} from 'react';
-import {AppContext} from '../context/AppContext';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Remaining = () => {
-    const {expenses, budget} = useContext(AppContext);
+    const { expenses, budget } = useContext(AppContext);
+
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
+
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
+    );
 };
-return (<div className={'alert ${alert-type}'}>
-            <span>Remaining; £{budget - totalExpenses}</span>
-        </div>);
 
 export default Remaining;
